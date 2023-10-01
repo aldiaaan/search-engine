8e61f9a4b43d1d2816c8807987a3cec7e343aac7
aldiaaaaan <aldian.asmr@gmail.com>
2023-10-01 12:29:19 +0700

    wip

---
+++ src/api/domain.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, request, jsonify
+from src.auth import Auth
+from src.common.errors import ServerException
+from src.domain import Domain
+import math
+
+bp_domain = Blueprint("domain", __name__)
+
+
+@bp_domain.route("/", methods=["GET"])
+def get_domains():
+    domains, total = Domain.find({
+        "limit": int(request.args.get("limit") or 10),
+        "start": int(request.args.get("start") or 0),
+        "sort_total_pages": request.args.get("sort_total_pages") or "DESC"
+    })
+    return jsonify({
+        "data": list(map(lambda x: x.to_dict(), domains)),
+        "pagination": {
+            "total": total,
+            "pages": math.ceil(total / int(request.args.get("limit") or 10)),
+            "current_page": math.floor(int(request.args.get("start") or 0) / int(request.args.get("limit") or 10))
+        }
+    })
