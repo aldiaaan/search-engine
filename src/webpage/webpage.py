ff32ed758546570b39c8c4606c346f9d394c3ff5
aldiaaaaan <aldian.asmr@gmail.com>
2023-10-04 02:33:56 +0700

    wip

--- src/webpage/webpage.py
+++ src/webpage/webpage.py
@@ -6,14 +6,16 @@ import pymysql
 
 class Webpage:
 
-    def __init__(self, title: str = None, url: str = None, pagerank_score: int = -1, keywords: tuple = tuple()):
+    def __init__(self, total_words: int = 0, title: str = None, url: str = None, pagerank_score: int = -1, keywords: tuple = tuple()):
         self.title = title
         self.keywords = keywords
         self.pagerank_score = pagerank_score
         self.url = url
+        self.total_words = total_words
 
     def to_dict(self):
         return {
+            "total_words": self.total_words,
             "title": self.title,
             "url": self.url,
             "keywords": self.keywords,
@@ -41,18 +43,18 @@ class Webpage:
         db = Database()
         connection = db.connect()
         cursor = connection.cursor(pymysql.cursors.DictCursor)
-        query = """SELECT * FROM page_information pi JOIN pagerank p ON pi.id_page = p.page_id WHERE pi.url LIKE '{}'  ORDER BY pagerank_score {}  LIMIT {} OFFSET {}""".format("%" + options["query"] + "%", options["sort_pagerank_score"], options["limit"], options["start"])
+        query = """SELECT *, COUNT(*) as total_words FROM page_information pi join tfidf_word tw on tw.page_id = pi.id_page JOIN pagerank p ON pi.id_page = p.page_id WHERE pi.url LIKE '{}' GROUP BY pi.url  ORDER BY pagerank_score {}  LIMIT {} OFFSET {}""".format("%" + options["query"] + "%", options["sort_pagerank_score"], options["limit"], options["start"])
 
         cursor.execute(query)
 
         webpages = cursor.fetchall()
 
         def mapper(page):
-            return Webpage(title=page.get("title"), url=page.get("url"), pagerank_score=page.get("pagerank_score"), keywords=page.get("keywords").split(","))
+            return Webpage(title=page.get("title"), url=page.get("url"), pagerank_score=page.get("pagerank_score"), keywords=page.get("keywords").split(","), total_words=page.get("total_words"))
 
         webpages = list(map(mapper, webpages))
 
-        query = "SELECT COUNT(*) as total FROM page_information pi JOIN pagerank p ON pi.id_page = p.page_id WHERE pi.url LIKE '%{}%'   "
+        query = "SELECT COUNT(*) as total FROM page_information pi JOIN pagerank p ON pi.id_page = p.page_id WHERE pi.url LIKE '{}'".format(options["query"])
         
         cursor.execute(query)
 
